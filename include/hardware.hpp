7477a6d1cd7e3e424ca9190bb900f0de6e515339
Daniel TB <varig203@gmail.com>
2025-03-07 20:47:07 -0800

    Removed unnecesary code

--- include/hardware.hpp
+++ include/hardware.hpp
@@ -6,21 +6,11 @@
 // Motor groups
 extern pros::MotorGroup left_motors;
 extern pros::MotorGroup right_motors;
-extern pros::Motor      primary_intake;
-extern pros::Motor      secondary_intake;
 
 // Controllers and Pistons
 extern pros::Controller      controller;
-extern pros::adi::DigitalOut pistonMogo;
-extern pros::adi::DigitalOut pistonLB;
-extern pros::adi::DigitalOut pistonDoink;
-
 // Sensors
 extern pros::Imu             imu;
-extern pros::Rotation        vertical_encoder;
-extern pros::Rotation        horizontal_encoder;
-extern lemlib::TrackingWheel vertical_tracking_wheel;
-extern lemlib::TrackingWheel horizontal_tracking_wheel;
 
 // Drivetrain settings
 extern lemlib::Drivetrain  drivetrain;
--- src/hardware.cpp
+++ src/hardware.cpp
@@ -4,46 +4,37 @@
 // Motor groups
 pros::MotorGroup left_motors({-1, -2, -3,4,5,6}, pros::MotorGearset::blue);
 pros::MotorGroup right_motors({7, 8, 9,10,11,12}, pros::MotorGearset::blue);
-pros::Motor      primary_intake(50, pros::MotorGearset::blue);            // Primary Intake on port 5
-pros::Motor      secondary_intake(50, pros::MotorGearset::blue);          // Secondary Intake on port 4
 
 // Controller and Pistons
-pros::Controller      controller(pros::E_CONTROLLER_MASTER); // Initialize controller
-pros::adi::DigitalOut pistonMogo('A');                       // Initialize the solenoid for MOGO mech
-pros::adi::DigitalOut pistonDoink('B');                               // Initalize the Doinker
-pros::adi::DigitalOut pistonLB('C');                         // Initialize the LB mech
+pros::Controller controller(pros::E_CONTROLLER_MASTER); // Initialize controller
 
 // Sensors
-pros::Imu             imu(50); // IMU
-pros::Rotation        vertical_encoder(50);
-pros::Rotation        horizontal_encoder(50);
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_2, 3);
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_2, 0);
+pros::Imu imu(20); // IMU
 
 // Drivetrain settings
 lemlib::Drivetrain  drivetrain(&left_motors,               // left motor group
                                &right_motors,              // right motor group
                                16,                         // 10 inch track width
                                lemlib::Omniwheel::NEW_325, // using new 4" omnis
-                               600,                        // drivetrain rpm is 360
+                               3600,                        // drivetrain rpm is 360
                                2                           // horizontal drift is 2 (for now)
  );
-lemlib::OdomSensors sensors(&vertical_tracking_wheel,   // Vertical tracking wheel
+lemlib::OdomSensors sensors(nullptr,   // Vertical tracking wheel
                             nullptr,                    // 2nd Vertical Tracking Wheel
-                            &horizontal_tracking_wheel, // Horizontal Tracking wheel
+                            nullptr, // Horizontal Tracking wheel
                             nullptr,                    // 2nd Horizontal tracking wheel
                             &imu                        // IMU Sensor
 );
 
 // PID controllers
-lemlib::ControllerSettings angular_controller(10,  // proportional gain (kP) 19
-                                              0.1,  // integral gain (kI)
-                                              1, // derivative gain (kD) 15
-                                              3,  // anti windup
-                                              0.1,  // small error range, in inches
-                                              100,  // small error range timeout, in milliseconds
-                                              1,  // large error range, in inches
-                                              500,  // large error range timeout, in milliseconds
+lemlib::ControllerSettings angular_controller(0,  // proportional gain (kP) 19
+                                              0,  // integral gain (kI)
+                                              0, // derivative gain (kD) 15
+                                              0,  // anti windup
+                                              0,  // small error range, in inches
+                                              0,  // small error range timeout, in milliseconds
+                                              0,  // large error range, in inches
+                                              0,  // large error range timeout, in milliseconds
                                               0   // maximum acceleration (slew)
 );
 lemlib::ControllerSettings lateral_controller(0, // proportional gain (kP)
